946553bfbef77bdfdfadd8e70aaca6f1ddd574ad
erlangamara <erlang@Erlangs-MacBook-Pro.local>
2019-12-18 19:37:14 +0700

    upload tugas

---
+++ exercise-6-looping-erlang-ahmad-amara.js
@@ -0,0 +1,68 @@
+//Number 1
+var count = 1;
+console.log('LOOPING PERTAMA');
+while(count < 21){
+    angka = count % 2;
+    if(angka === 0){
+        console.log(count + ' - I love coding');
+    }
+    count++;
+}
+
+count2 = 20;
+console.log('LOOPING KEDUA');
+while(count2 > 1){
+    angka = count2 % 2;
+    if(angka === 0){
+        console.log(count2 + ' - I will become fullstack developer');
+    }
+    count2--;
+}
+
+//Number 2
+/*console.log('LOOPING PERTAMA');
+for(count = 1; count < 21; count++){
+        console.log(count + ' - I love coding');
+}
+
+count2 = 20;
+console.log('LOOPING KEDUA');
+for(count2 = 20; count2 >= 1; count2--){
+        console.log(count2 + ' - fullstack developer');
+}*/
+
+//Number 3
+//ganjil genap
+/*for(counter1 = 1; counter1 < 101; counter1++){
+    selector = counter1 % 2;
+    if(selector === 0){
+        console.log('GENAP');
+    }
+    else{
+        console.log('GANJIL');
+    }
+}*/
+
+//pertambahan counter 2
+/*for(counter2 = 1; counter2 < 101; counter2 += 2){
+    selector2 = counter2 % 3;
+    if(selector2 === 0){
+        console.log(counter2 + ' KELIPATAN 3');
+    } 
+}*/
+
+//pertambahan counter 5
+/*for(counter3 = 1; counter3 < 101; counter3 += 5){
+    selector3 = counter3 % 6;
+    if(selector3 === 0){
+        console.log(counter3 + ' KELIPATAN 6');
+    } 
+}*/
+
+//pertambahan counter 9
+/*for(counter4 = 1; counter4 < 101; counter4 += 9){
+    selector4 = counter4 % 10;
+    if(selector4 === 0){
+        console.log(counter4 + ' KELIPATAN 10');
+    } 
+}*/
